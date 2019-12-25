e88540b794e5d8735e7ea66ecf9fc3d2d10f3229
mpiuser <rajiniw95@gmail.com>
2019-12-25 23:11:54 +0530

    dynamic thread fixed

--- controlThread.go
+++ controlThread.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"strconv"
 	"encoding/csv"
+	"reflect"
 )
 
 func pythonCall(progName string, inChannel chan <- string, workflowNumber string) {
@@ -94,14 +95,15 @@ func main(){
 	go messagePassing(outChannelModule2, outChannelModule3)
 	fmt.Println(<- outChannelModule3)
 
+//JOHN
 	// Open the file
-	csvfile, err := os.Open("/home/mpiuser/Desktop/multiplyByTwo.csv")
-	if err != nil {
-		log.Fatalln("Couldn't open the csv file", err)
+	csvfile1, err1 := os.Open("/home/mpiuser/Desktop/multiplyByTwo.csv")
+	if err1 != nil {
+		log.Fatalln("Couldn't open the csv file", err1)
 	}
 
 	// Parse the file
-	r := csv.NewReader(csvfile)
+	r1 := csv.NewReader(csvfile1)
 	//r := csv.NewReader(bufio.NewReader(csvfile))
 
 	// Iterate through the records
@@ -110,16 +112,16 @@ func main(){
 	var count = 0
 	for {
 		// Read each record from csv
-		record, err := r.Read()
-		if err == io.EOF {
+		record1, err1 := r1.Read()
+		if err1 == io.EOF {
 			break
 		}
-		if err != nil {
-			log.Fatal(err)
+		if err1 != nil {
+			log.Fatal(err1)
 		}
 		//fmt.Printf(record[0])
 		
-		studentMarks[count]=record[0]
+		studentMarks[count]=record1[0]
 		//fmt.Println(count)
 		count = count + 1
 	}
@@ -142,15 +144,88 @@ func main(){
 	for _, num := range studentMarksInt {
         	studentMarksTotal += num
     	}
-    	//fmt.Println("sum:", studentMarksTotal)
+    	fmt.Println(studentMarksTotal)
+	fmt.Println(reflect.TypeOf(studentMarksTotal))
+
+//CLASS
+	// Open the file
+	csvfile2, err2 := os.Open("/home/mpiuser/Desktop/calculateAverage.csv")
+	if err2 != nil {
+		log.Fatalln("Couldn't open the csv file", err2)
+	}
+
+	// Parse the file
+	r2 := csv.NewReader(csvfile2)
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+
+	var classAverage string
+	for {
+		// Read each record from csv
+		record2, err2 := r2.Read()
+		if err2 == io.EOF {
+			break
+		}
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		//fmt.Printf(record[0])
+		
+		classAverage=record2[0]
+		//fmt.Println(count)
+		//count = count + 1
+	}
+
+	fmt.Println(classAverage)
+	fmt.Println(reflect.TypeOf(classAverage))
+
+	//var s float64
+
+	classAverageInt, _ := strconv.ParseFloat(classAverage, 8)
+	fmt.Println(classAverageInt)
+	fmt.Println(reflect.TypeOf(classAverageInt))
 
 	outChannelModule4 := make(chan string, 1)
-	go pythonCall("workflow/"+commandsArray[3], outChannelModule4, "1")
-	go messagePassing(outChannelModule3, outChannelModule4)
-	fmt.Println(<- outChannelModule4)
 
-	outChannelModule5 := make(chan string, 1)
-	go pythonCall("workflow/"+commandsArray[4], outChannelModule5, "1")
-	go messagePassing(outChannelModule4, outChannelModule5)
-	fmt.Println(<- outChannelModule5)
+	if studentMarksTotal >= int(classAverageInt) {
+		go pythonCall("workflow/"+commandsArray[3], outChannelModule4, "1")
+		go messagePassing(outChannelModule3, outChannelModule4)
+		fmt.Println(<- outChannelModule4)
+    	} else {
+		go pythonCall("workflow/"+commandsArray[4], outChannelModule4, "1")
+		go messagePassing(outChannelModule3, outChannelModule4)
+		fmt.Println(<- outChannelModule4)
+   	}
+	
+/*
+	if s, err := strconv.ParseFloat(classAverage, 32); err == nil {
+    		fmt.Println(s)
+		fmt.Println(reflect.TypeOf(s))
+		
+	}
+	fmt.Println(s)
+
+
+	var classAverageInt = []int{}
+
+	for _, i := range classAverage {
+        	j, err := strconv.Atoi(i)
+        	if err != nil {
+            		panic(err)
+        	}
+        	classAverageInt = append(classAverageInt, j)
+    	}
+    	//fmt.Println(studentMarksInt)
+
+	var studentMarksTotal int
+	
+	for _, num := range studentMarksInt {
+        	studentMarksTotal += num
+    	}
+    	//fmt.Println("sum:", studentMarksTotal)*/
+
+	
+
+	
 }
