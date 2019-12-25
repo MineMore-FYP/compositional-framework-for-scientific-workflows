ec654152e8b0f88567116033193ae1a764081990
mpiuser <rajiniw95@gmail.com>
2019-12-25 17:19:26 +0530

    implicit parallelism handled

--- workflow/WFComponent2/calculateAvg.py
+++ workflow/WFComponent2/calculateAvg.py
@@ -12,21 +12,31 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 import userScript
 
-currentModule = "module1"
+classMarks = userScript.classMarks
+outputLocation = userScript.outputLocation
 
-workflowNumber = sys.argv[1]
+mulTwoArray = []
 
-if workflowNumber == "1":
-	orderOfModules = userScript.orderOfModules1
-	classMarks = userScript.classMarks1
-	marks = userScript.marks1
+# open file and read the content in a list
+with open(outputLocation+'multiplyByTwo.txt', 'r') as f:
+    for line in f:
+        # remove linebreak which is the last character of the string
+        mark = line[:-1]
+        # add item to the list
+        mulTwoArray.append(mark)
+
+studentTotal = 0
+for j in mulTwoArray:
+	studentTotal += int(j)
+
+classMarks.append(studentTotal)
 
 @python_app
 def classAverage ():
-    classMarks.append(marks)
     average = sum(classMarks) / len(classMarks)
     return average
 
 print(classAverage().result())
 
 print("Module Completed : Calculate Class Average")
+
---
+++ workflow/WFComponent2/multiplyByTwo.py
@@ -0,0 +1,33 @@
+import parsl
+import os
+from parsl.app.app import python_app, bash_app
+from parsl.configs.local_threads import config
+
+parsl.load(config)
+
+import sys
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+import userScript
+
+studentMarks = userScript.studentMarks
+classMarks = userScript.classMarks
+outputLocation = userScript.outputLocation
+
+@python_app
+def multiplyByTwo (x):
+    multiplyByTwo = x*2
+    return multiplyByTwo
+
+mulTwoArray = []
+for i in studentMarks:
+	mulTwoArray.append(multiplyByTwo(i).result())
+
+with open(outputLocation+'multiplyByTwo.txt', 'w') as f:
+    for k in mulTwoArray:
+        f.write("%s\n" % k)
+
+print("Module Completed : Multiply Marks by 2 Complete")
+
